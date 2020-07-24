dc8e5a7216f3196d6fec6bb06e2b7ffd2f436233
GreedSource <greedsource@outlook.com>
2020-07-24 09:44:56 -0500

    cadena de conexión de base de datos Heroku

--- models/connection.js
+++ models/connection.js
@@ -3,11 +3,11 @@ port = process.env.PORT || 4205;
 
 if (port === 4205){
     var connection = mysql.createConnection({
-        host: 'localhost',
+        host: 'us-cdbr-east-02.cleardb.com',
         port:3306,
-        user:'root',
-        password: '',
-        database:'nodecommerce',
+        user:'bb26f1598c0e8e',
+        password: 'fac0cf03',
+        database:'heroku_ccbf50efafee06f',
         insecureAuth: true
     });
 }else{
